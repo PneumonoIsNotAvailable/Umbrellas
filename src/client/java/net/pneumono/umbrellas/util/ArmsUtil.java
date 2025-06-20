6fcc05cbebd02255f76243334f21b74017ffd253
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-06-20 05:46:14 +0100

    Improve arm adjustment logic

--- src/client/java/net/pneumono/umbrellas/util/ArmsUtil.java
+++ src/client/java/net/pneumono/umbrellas/util/ArmsUtil.java
@@ -1,10 +1,13 @@
 package net.pneumono.umbrellas.util;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.state.BipedEntityRenderState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.pneumono.umbrellas.registry.UmbrellasTags;
 
 import java.util.function.Predicate;
@@ -30,10 +33,25 @@ public class ArmsUtil {
 
     public static boolean shouldAdjustArm(Arm arm, LivingEntity entity) {
         ItemStack stack = entity.getStackInArm(arm);
-        if (stack.isIn(UmbrellasTags.UMBRELLAS)) {
-            return entity.fallDistance > 2;
+        if (!stack.isIn(UmbrellasTags.UMBRELLAS)) return false;
+
+        World world = entity.getWorld();
+        BlockPos pos = entity.getBlockPos();
+
+        for (int i = 0; i < 2; ++i) {
+            BlockState state = world.getBlockState(pos.down(i));
+            if (!state.getCollisionShape(world, pos).isEmpty()) {
+                return false;
+            }
         }
-        return false;
+
+        if (UmbrellaUtils.getSlowFallingStrength(stack, entity.getRandom()) > 0) {
+            if (entity.fallDistance > 1.25) return true;
+        }
+        if (UmbrellaUtils.getSmokeBoostingStrength(stack, entity.getRandom()) > 0) {
+            if (UmbrellaUtils.isInSmoke(world, pos)) return true;
+        }
+        return entity.fallDistance > 2.25;
     }
 
     public static boolean shouldAdjustArm(Arm arm, UmbrellaHoldingEntityRenderState state) {
