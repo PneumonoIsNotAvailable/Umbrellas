f7258d4bbe93cd898bb489e4c3dbb31e9e8a0c62
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2025-08-19 02:55:48 +0100

    Update configs to use new API

--- src/main/java/net/pneumono/umbrellas/UmbrellasConfig.java
+++ src/main/java/net/pneumono/umbrellas/UmbrellasConfig.java
@@ -1,41 +1,38 @@
 package net.pneumono.umbrellas;
 
-import net.pneumono.pneumonocore.config.*;
+import net.pneumono.pneumonocore.config_api.ConfigApi;
+import net.pneumono.pneumonocore.config_api.configurations.*;
+import net.pneumono.pneumonocore.config_api.enums.LoadType;
 import net.pneumono.umbrellas.util.EnchantmentAbilityType;
 
 public class UmbrellasConfig {
-    public static final String MOD_ID = Umbrellas.MOD_ID;
+    public static final EnumConfiguration<EnchantmentAbilityType> SLOW_FALLING = register("slow_falling", new EnumConfiguration<>(
+            EnchantmentAbilityType.ENCHANTED_ONLY, new ConfigSettings().category("umbrellas").loadType(LoadType.INSTANT)
+    ));
+    public static final EnumConfiguration<EnchantmentAbilityType> SMOKE_BOOSTING = register("smoke_boosting", new EnumConfiguration<>(
+            EnchantmentAbilityType.ENCHANTED_ONLY, new ConfigSettings().category("umbrellas").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration STRICT_SMOKE_BOOSTING = register("strict_smoke_boosting", new BooleanConfiguration(
+            false, new ConfigSettings().category("umbrellas").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration DURABILITY = register("durability", new BooleanConfiguration(
+            true, new ConfigSettings().category("umbrellas").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration DISABLE_FIREWORK_BOOSTING = register("disable_firework_boosting", new BooleanConfiguration(
+            false, new ConfigSettings().category("elytra_changes").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration ELYTRA_SMOKE_BOOSTING = register("elytra_smoke_boosting", new BooleanConfiguration(
+            false, new ConfigSettings().category("elytra_changes").loadType(LoadType.INSTANT)
+    ));
+    public static final BooleanConfiguration ENCHANTMENT_GLINT = register("enchantment_glint", new BooleanConfiguration(
+            true, new ConfigSettings().clientSide().category("umbrellas").loadType(LoadType.INSTANT)
+    ));
 
-    public static final EnumConfiguration<EnchantmentAbilityType> SLOW_FALLING = new EnumConfiguration<>(MOD_ID, "slow_falling", ConfigEnv.SERVER, EnchantmentAbilityType.ENCHANTED_ONLY);
-    public static final EnumConfiguration<EnchantmentAbilityType> SMOKE_BOOSTING = new EnumConfiguration<>(MOD_ID, "smoke_boosting", ConfigEnv.SERVER, EnchantmentAbilityType.ENCHANTED_ONLY);
-    public static final BooleanConfiguration STRICT_SMOKE_BOOSTING = new BooleanConfiguration(MOD_ID, "strict_smoke_boosting", ConfigEnv.SERVER, false);
-    public static final BooleanConfiguration DURABILITY = new BooleanConfiguration(MOD_ID, "durability", ConfigEnv.SERVER, true);
-    public static final BooleanConfiguration DISABLE_FIREWORK_BOOSTING = new BooleanConfiguration(MOD_ID, "disable_firework_boosting", ConfigEnv.SERVER, false);
-    public static final BooleanConfiguration ELYTRA_SMOKE_BOOSTING = new BooleanConfiguration(MOD_ID, "elytra_smoke_boosting", ConfigEnv.SERVER, false);
-    public static final BooleanConfiguration ENCHANTMENT_GLINT = new BooleanConfiguration(MOD_ID, "enchantment_glint", ConfigEnv.CLIENT, true);
+    public static <T extends AbstractConfiguration<?>> T register(String name, T config) {
+        return ConfigApi.register(Umbrellas.id(name), config);
+    }
 
     public static void registerUmbrellasConfigs() {
-        Configs.register(MOD_ID,
-                SLOW_FALLING,
-                SMOKE_BOOSTING,
-                STRICT_SMOKE_BOOSTING,
-                DURABILITY,
-                DISABLE_FIREWORK_BOOSTING,
-                ELYTRA_SMOKE_BOOSTING,
-                ENCHANTMENT_GLINT
-        );
-        Configs.registerCategories(MOD_ID,
-                new ConfigCategory(MOD_ID, "umbrellas",
-                        SLOW_FALLING,
-                        SMOKE_BOOSTING,
-                        STRICT_SMOKE_BOOSTING,
-                        DURABILITY,
-                        ENCHANTMENT_GLINT
-                ),
-                new ConfigCategory(MOD_ID, "elytra_changes",
-                        DISABLE_FIREWORK_BOOSTING,
-                        ELYTRA_SMOKE_BOOSTING
-                )
-        );
+        ConfigApi.finishRegistry(Umbrellas.MOD_ID);
     }
 }
