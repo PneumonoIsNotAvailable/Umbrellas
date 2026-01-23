8f95f22364ca06eab4c104f3d4f12993d143ab07
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2026-01-23 04:29:01 +0000

    Block entity renderer improvements

--- src/client/java/net/pneumono/umbrellas/content/UmbrellaStandBlockEntityRenderer.java
+++ src/client/java/net/pneumono/umbrellas/content/UmbrellaStandBlockEntityRenderer.java
@@ -5,6 +5,7 @@ import com.mojang.math.Axis;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.pneumono.umbrellas.content.block.UmbrellaStandBlock;
 import net.pneumono.umbrellas.content.block.UmbrellaStandBlockEntity;
@@ -12,6 +13,7 @@ import net.pneumono.umbrellas.content.block.UmbrellaStandBlockEntity;
 //? if >=1.21.9 {
 import it.unimi.dsi.fastutil.HashCommon;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.client.renderer.item.ItemStackRenderState;
@@ -21,31 +23,16 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 //?} else {
 /*import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.geom.EntityModelSet;
-import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.pneumono.umbrellas.content.item.component.UmbrellaPatternsComponent;
-import net.pneumono.umbrellas.registry.UmbrellasDataComponents;
-import net.pneumono.umbrellas.registry.UmbrellasTags;
-import java.util.Objects;
 *///?}
 
-public class UmbrellaStandBlockEntityRenderer implements BlockEntityRenderer<UmbrellaStandBlockEntity/*? if >=1.21.9 {*/, UmbrellaRenderState/*?}*/> {
-    //? if >=1.21.9 {
+public class UmbrellaStandBlockEntityRenderer implements BlockEntityRenderer<UmbrellaStandBlockEntity/*? if >=1.21.9 {*/, UmbrellaStandBlockEntityRenderer.UmbrellaRenderState/*?}*/> {
+    //? if >=1.21.9
     private final ItemModelResolver itemModelResolver;
-    //?} else {
-    /*private final UmbrellaRenderer renderer;
-    *///?}
 
     public UmbrellaStandBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
-        //? if >=1.21.9 {
+        //? if >=1.21.9
         this.itemModelResolver = context.itemModelResolver();
-        //?} else {
-        /*EntityModelSet set = context.getModelSet();
-        this.renderer = new UmbrellaRenderer(set);
-        *///?}
     }
 
     //? if >=1.21.9 {
@@ -61,14 +48,14 @@ public class UmbrellaStandBlockEntityRenderer implements BlockEntityRenderer<Umb
         this.itemModelResolver.updateForTopItem(state.item, entity.getTheItem(), ItemDisplayContext.NONE, entity.getLevel(), null, HashCommon.long2int(entity.getBlockPos().asLong()));
     }
 
+    public static class UmbrellaRenderState extends BlockEntityRenderState {
+        public ItemStackRenderState item;
+    }
+
     @Override
     public void submit(UmbrellaRenderState state, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState cameraRenderState) {
         poseStack.pushPose();
-        poseStack.translate(0.5, 0.0, 0.5);
-        float g = state.blockState.getValue(UmbrellaStandBlock.FACING).toYRot();
-        poseStack.mulPose(Axis.YP.rotationDegrees(-g));
-        //poseStack.translate(-0.5, 0.0, -0.5);
-        poseStack.translate(0.03125, 1.5, 0.03125);
+        adjustPoseStack(poseStack, state.blockState);
         state.item.submit(poseStack, submitNodeCollector, state.lightCoords, OverlayTexture.NO_OVERLAY, 0);
         poseStack.popPose();
     }
@@ -76,43 +63,24 @@ public class UmbrellaStandBlockEntityRenderer implements BlockEntityRenderer<Umb
     //?} else {
     /*@Override
     public void render(UmbrellaStandBlockEntity entity, float tickProgress, PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay, Vec3 vec3) {
-        ItemStack stack = entity.getTheItem();
-        if (stack.isEmpty() || !stack.is(UmbrellasTags.UMBRELLAS)) return;
-
-        int lightAbove = LevelRenderer.getLightColor(Objects.requireNonNull(entity.getLevel()), entity.getBlockPos().above(2));
-
         poseStack.pushPose();
-        poseStack.translate(0.5, 0.0, 0.5);
-        float g = entity.getBlockState().getValue(UmbrellaStandBlock.FACING).toYRot();
-        poseStack.mulPose(Axis.YP.rotationDegrees(-g));
-        poseStack.translate(-0.5, 0.0, -0.5);
-        poseStack.translate(0.03125, 1.0, 0.03125);
-        if (stack.getComponents().has(UmbrellasDataComponents.UMBRELLA_PATTERNS)) {
-            renderPatternable(poseStack, multiBufferSource, lightAbove, overlay, stack);
-        } else {
-            renderNonPatternable(poseStack, multiBufferSource, lightAbove, overlay, stack, entity.getLevel());
-        }
-        poseStack.popPose();
-    }
+        adjustPoseStack(poseStack, entity.getBlockState());
 
-    private void renderPatternable(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay, ItemStack stack) {
-        this.renderer.submit(
-                poseStack, multiBufferSource,
-                light, overlay,
-                stack.hasFoil(), 0.0F,
-                stack.getOrDefault(UmbrellasDataComponents.UMBRELLA_PATTERNS, UmbrellaPatternsComponent.DEFAULT)
-        );
-    }
-
-    private void renderNonPatternable(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay, ItemStack stack, Level level) {
-        poseStack.translate(0.5, 0.5, 0.5);
         Minecraft.getInstance().getItemRenderer().renderStatic(
-                stack,
+                entity.getTheItem(),
                 ItemDisplayContext.NONE,
                 light, overlay,
                 poseStack, multiBufferSource,
-                level, 0
+                entity.getLevel(), 0
         );
+        poseStack.popPose();
     }
     *///?}
+
+    public static void adjustPoseStack(PoseStack poseStack, BlockState state) {
+        poseStack.translate(0.5, 0.0, 0.5);
+        float g = state.getValue(UmbrellaStandBlock.FACING).toYRot();
+        poseStack.mulPose(Axis.YP.rotationDegrees(-g));
+        poseStack.translate(0.03125, 1.5, 0.03125);
+    }
 }
