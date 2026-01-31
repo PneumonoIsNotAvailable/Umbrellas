ece6809bed4a05c2b0b3936baaa8c7321b0243eb
Pneumono_ <107329140+PneumonoIsNotAvailable@users.noreply.github.com>
2026-01-31 05:38:27 +0000

    Arm animation condition adjustments

--- src/client/java/net/pneumono/umbrellas/util/ArmsUtil.java
+++ src/client/java/net/pneumono/umbrellas/util/ArmsUtil.java
@@ -3,12 +3,9 @@ package net.pneumono.umbrellas.util;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import net.pneumono.umbrellas.registry.UmbrellasTags;
 
 //? if >=1.21.6
@@ -54,26 +51,11 @@ public class ArmsUtil {
     public static boolean shouldAdjustArm(HumanoidArm arm, LivingEntity entity) {
         ItemStack stack = entity.getMainArm() == arm ? entity.getMainHandItem() : entity.getOffhandItem();
         if (!stack.is(UmbrellasTags.UMBRELLAS)) return false;
-        if (entity.getDeltaMovement().y > 0.1 || entity.getDeltaMovement().y < -0.1) return true;
-
-        Level level = entity.level();
-        BlockPos pos = entity.blockPosition();
-
-        for (int i = 0; i < 4; ++i) {
-            BlockState blockState = level.getBlockState(pos.below(i));
-            if (!blockState.getCollisionShape(level, pos).isEmpty()) {
-                return false;
-            }
-        }
 
         int slowFallStrength = UmbrellaUtils.getSlowFallingStrength(stack, entity.getRandom());
-        if (slowFallStrength > 0) {
-            if (entity.fallDistance > Math.max(1.25 - (slowFallStrength * 0.25), 0)) return true;
-        }
-        if (UmbrellaUtils.hasSmokeBoosting(stack, entity.getRandom())) {
-            if (UmbrellaUtils.isVisuallyInSmoke(level, pos)) return true;
-        }
-        return entity.fallDistance > 1.25;
+        if (entity.fallDistance > Math.max(4.25 - slowFallStrength, 0)) return true;
+
+        return UmbrellaUtils.hasSmokeBoosting(stack, entity.getRandom()) && UmbrellaUtils.isVisuallyInSmoke(entity.level(), entity.blockPosition());
     }
 
     public static void adjustArm(PoseStack poseStack) {
